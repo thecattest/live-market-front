8c18d0b4e30659504b34f33e366b630f43097c93
DSTU Hackathon Spring 2021 <hackathon@451407-ca74341.tmweb.ru>
2021-03-21 15:55:55 +0300

    products

---
+++ components/Product.vue
@@ -0,0 +1,47 @@
+<!-- eslint-disable prettier/prettier, max-len -->
+<template>
+  <li class="product">
+    <img
+      :src="product.image"
+      alt=""
+      class="product__image"
+    />
+    <h3>{{ product.name }}</h3>
+    <span>{{ product.price }}</span>
+    <input type="checkbox" />
+  </li>
+</template>
+
+<script>
+  export default {
+    props: {
+      product: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style lang="less">
+  .product {
+    display: grid;
+    grid-column: 20px;
+    align-items: center;
+    justify-items: center;
+    justify-content: center;
+    align-content: center;
+
+    &__image {
+      width: 300px;
+    }
+
+    h3 {
+      margin-bottom: 30px;
+    }
+
+    span {
+      font-size: 34px;
+    }
+  }
+</style>
---
+++ pages/customer.vue
@@ -0,0 +1,45 @@
+<!-- eslint-disable prettier/prettier, max-len -->
+<template>
+  <div class="customer">
+    <div class="container">
+      <ul class="products">
+        <Product
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        products: {},
+      };
+    },
+    mounted() {
+      this.getProducts();
+    },
+    methods: {
+      async getProducts() {
+        import(`~/products.js`).then((data) => {
+          this.products = data;
+        });
+      },
+    },
+  };
+</script>
+
+<style lang="less">
+  .products {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+
+  .customer {
+    padding: 100px 0;
+  }
+</style>
